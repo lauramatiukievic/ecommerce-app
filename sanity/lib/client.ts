59944568f03ea8d20154300707f61c1aa8311de3
Dominykas Kisonas <dominykas.kisonas@citadele.lv>
2023-07-24 18:54:57 +0300

    seeded product data

--- sanity/lib/client.ts
+++ sanity/lib/client.ts
@@ -7,4 +7,5 @@ export const client = createClient({
   dataset,
   projectId,
   useCdn,
+  token: "sk5y0YJOBaLRzuOCQvNEmtX8O1mqAY5CXnsTQ8KN25rW5ZCZhzK1R5Y8wh3qZvf1F5uCQpssFCY3mykNKB72vKO7S5cGwdIBhfjj49fcVJuTAs0DONJTTW3Qd6cbX6CkMdUInUBOHqCwSyXxKm07SNeLOeZu5O5uQO7H16h4EmQAC7uFOD1p"
 })
