7de4c502d5662db3141ecda1cb1dec014d9b29ee
Dominykas Kisonas <dominykas.kisonas@citadele.lv>
2023-07-24 18:58:29 +0300

    querying products with groq

--- app/page.tsx
+++ app/page.tsx
@@ -1,16 +1,29 @@
-import { client } from "@/sanity/lib/client"
-import { groq } from "next-sanity"
+import {client} from "@/sanity/lib/client"
+import {groq} from "next-sanity"
 
-import { SanityProduct } from "@/config/inventory"
-import { siteConfig } from "@/config/site"
-import { cn } from "@/lib/utils"
-import { ProductFilters } from "@/components/product-filters"
-import { ProductGrid } from "@/components/product-grid"
-import { ProductSort } from "@/components/product-sort"
+import {SanityProduct} from "@/config/inventory"
+import {siteConfig} from "@/config/site"
+import {cn} from "@/lib/utils"
+import {ProductFilters} from "@/components/product-filters"
+import {ProductGrid} from "@/components/product-grid"
+import {ProductSort} from "@/components/product-sort"
 
-interface Props {}
+interface Props {
+}
 
-export default function Page() {
+export default async function Page() {
+  const products = await client.fetch<SanityProduct[]>(groq`*[_type == "product"] {
+      _id,
+      _createdAt,
+      name,
+      sku,
+      images,
+      currency,
+      price,
+      description,
+      "slug": slug.current
+  }`)
+  console.log(products)
   return (
     <div>
       <div className="px-4 pt-20 text-center">
