55166ef1a968c84eef6b27b616c351562b873868
Laura Matiukievic <laura.matiukievic@gmail.com>
2023-07-27 15:27:30 +0300

    finnal

--- sanity/lib/client.ts
+++ sanity/lib/client.ts
@@ -7,6 +7,4 @@ export const client = createClient({
   dataset,
   projectId,
   useCdn,
-  token:
-    "skPBGRJFw5FCM3IqFIzHALjPQVPSDGSisyMBmxYkETa576dQcmhGKLYpcqSnl6JmC3m1hLRApYRxpGgQcxhrh7ltvnmnU56yojec0Ff5DxCcdybNYND3fQIUFPNFFFETdDlwfOBvNvjEXQKtjXzB9YOQWU24eY66pvQr1JARyBOxJ5AFtL7I",
 })
